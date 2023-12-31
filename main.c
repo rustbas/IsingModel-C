3b5e83e6389626211dd0ca963d1eb1f3f8cc9cab
rustbas <hrustbas@gmail.com>
2023-12-31 11:33:00 +0000

    start developing

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
+#define NROWS 10
+#define NCOLS 15
+
 int main(int argc, char** argv){
+
+    int state[2][NROWS][NCOLS] = {0};
+
     printf("Hello, world!\n");
 
     return 0;
