e0368fc7633c4c69237050a297ba8d54040da349
rustbas <hrustbas@gmail.com>
2023-12-31 12:34:18 +0000

    add `printState` function

--- main.c
+++ main.c
@@ -1,13 +1,34 @@
-#include <stdio.h>
 
-#define NROWS 10
-#define NCOLS 15
+#include "isingModel.h"
+
+#define NROW 15
+#define NCOL 35
 
 int main(int argc, char** argv){
 
-    int state[2][NROWS][NCOLS] = {0};
+    int i, j;
 
-    printf("Hello, world!\n");
+    int** state[2];
+    state[0] = (int**) malloc(NROW * sizeof(int*));
+    for (i=0; i<NROW; ++i) {
+        state[0][i] = (int*) malloc(NCOL * sizeof(int));
+    }
+
+    state[1] = (int**) malloc(NROW * sizeof(int*));
+    for (i=0; i<NROW; ++i) {
+        state[1][i] = (int*) calloc(NCOL, sizeof(int));
+    }
+
+    for (i=0; i<NROW; ++i) {
+        for (j=0; j<NCOL; ++j) {
+            state[0][i][j] = 2 * (rand()%2) - 1;
+            state[1][i][j] = state[0][i][j];
+        }
+    }
+
+    /*printf("Hello, world!\n");*/
+
+    printState(state[0], NROW, NCOL);
 
     return 0;
 }
